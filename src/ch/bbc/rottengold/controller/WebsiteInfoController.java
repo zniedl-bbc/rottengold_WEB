c22448ff3e6797d304932649bf19bb9de362990e
zniedl-bbc <Luca.Niederer@bbcag.ch>
2016-11-28 16:02:57 +0100

    rating

--- src/ch/bbc/rottengold/controller/WebsiteInfoController.java
+++ src/ch/bbc/rottengold/controller/WebsiteInfoController.java
@@ -1,21 +1,18 @@
 package ch.bbc.rottengold.controller;
 
 import java.io.Serializable;
-
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
-import javax.enterprise.context.SessionScoped;
-import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
-import javax.servlet.http.HttpServletRequest;
-
+import ch.bbc.rottengold.ejb.RatingBeanLocal;
 import ch.bbc.rottengold.ejb.WebsiteInfoBeanLocal;
 import ch.bbc.rottengold.model.Rating;
 import ch.bbc.rottengold.model.Website;
 
 @Named
-@SessionScoped
+@ViewScoped
 public class WebsiteInfoController implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -23,21 +20,106 @@ public class WebsiteInfoController implements Serializable {
 	@EJB
 	private WebsiteInfoBeanLocal websiteInfoBean;
 
+	@EJB
+	private RatingBeanLocal ratingBean;
+
 	@Inject
 	private Website currentWebsite;
 
+	@Inject
+	private CommentController commentController;
+
 	private Rating rating;
 
-	public void setCurrentWebsiteFromId() {
-		HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext()
-				.getRequest();
-		String idURLParam = request.getParameter("id");
-		currentWebsite = websiteInfoBean.getWebsiteInfo(idURLParam);
+	private Rating[] allRatingsForCurrentWebsite;
 
+	@PostConstruct
+	public void init() {
+		currentWebsite = websiteInfoBean.getWebsiteInfo("" + commentController.getWebsiteId());
+		setRating(ratingBean.getRatingForWebsiteWithUser(commentController.getWebsiteId(),
+				commentController.getUserController().getUser().getId()));
+		setAllRatingsForCurrentWebsite(ratingBean.getAllRatingsForWebsite(commentController.getWebsiteId()));
+	}
+
+	public double getAverageRating() {
+		int counter = 0;
+		double averageRating = 0.0;
+
+		try {
+			for (Rating r : allRatingsForCurrentWebsite) {
+				averageRating += r.getRating();
+				counter++;
+			}
+		} catch (NullPointerException e) {
+			
+		}
+
+		averageRating = averageRating / counter;
+
+		return averageRating;
+	}
+
+	public boolean isIt1StarRating() {
+		if (getAverageRating() > 0.5) {
+			return true;
+		}
+		return false;
+	}
+
+	public boolean isIt2StarRating() {
+		if (getAverageRating() > 1.5) {
+			return true;
+		}
+		return false;
+	}
+
+	public boolean isIt3StarRating() {
+		if (getAverageRating() > 2.5) {
+			return true;
+		}
+		return false;
+	}
+
+	public boolean isIt4StarRating() {
+		if (getAverageRating() > 3.5) {
+			return true;
+		}
+		return false;
+	}
+
+	public boolean isIt5StarRating() {
+		if (getAverageRating() > 4.5) {
+			return true;
+		}
+		return false;
+	}
+
+	public void set1StarRatingForUser() {
+		ratingBean.setNewRatingForThisUser(1, commentController.getUserController().getUser().getId(),
+				commentController.getWebsiteId());
+	}
+
+	public void set2StarRatingForUser() {
+		ratingBean.setNewRatingForThisUser(2, commentController.getUserController().getUser().getId(),
+				commentController.getWebsiteId());
+	}
+
+	public void set3StarRatingForUser() {
+		ratingBean.setNewRatingForThisUser(3, commentController.getUserController().getUser().getId(),
+				commentController.getWebsiteId());
+	}
+
+	public void set4StarRatingForUser() {
+		ratingBean.setNewRatingForThisUser(4, commentController.getUserController().getUser().getId(),
+				commentController.getWebsiteId());
+	}
+
+	public void set5StarRatingForUser() {
+		ratingBean.setNewRatingForThisUser(5, commentController.getUserController().getUser().getId(),
+				commentController.getWebsiteId());
 	}
 
 	public Website getCurrentWebsite() {
-		setCurrentWebsiteFromId();
 		return currentWebsite;
 	}
 
@@ -51,6 +133,18 @@ public class WebsiteInfoController implements Serializable {
 
 	public void setRating(Rating rating) {
 		this.rating = rating;
+	}
+
+	public boolean isRatingAvailable() {
+		return true;
+	}
+
+	public Rating[] getAllRatingsForCurrentWebsite() {
+		return allRatingsForCurrentWebsite;
+	}
+
+	public void setAllRatingsForCurrentWebsite(Rating[] allRatingsForCurrentWebsite) {
+		this.allRatingsForCurrentWebsite = allRatingsForCurrentWebsite;
 	}
 
 }
